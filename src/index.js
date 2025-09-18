407ff2f9860696d0ddb2322e3f86d7a11ad0e1dc
Lebohang <llebo829@email.com>
2025-09-18 18:40:48 +0200

    Added API Integration to the Javascript

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function searchPoem(event) {
-  event.preventDefault();
-
+function showPoem(response) {
   new Typewriter("#poem-format", {
-    strings: "Les oiseaux chantent joyeusement",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function searchPoem(event) {
+  event.preventDefault();
+
+  let stipulationElement = document.querySelector("#user-stipulations");
+  let apiKey = "410o3ft86210d5f3d73f24a4d34d2bab";
+  let prompt = `Generate a french poem about ${stipulationElement.value}`;
+  let context =
+    "You are a French poet. Write exactly 4 lines. Make sure the output is formatted in a poem structure with each line separated by <br>. Do not add extra text or explanations, only the poem itself.Add a <strong>Shecodes AI</strong> signature at the end of the poem";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showPoem);
 }
 
 let poemGenerator = document.querySelector("#poem-form");
